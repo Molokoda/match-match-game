2a0ab3f9eb77f560e597b45eb73303056c387677
Molokoda <prototipich@gmail.com>
2020-09-23 02:37:02 +0300

    release v1.0

---
+++ precache-manifest.5b256227f0be77b43e2b282eb8b32212.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cc3761dee220ef0aba5c1e1ba39313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98453eddcfa6f15aeffa",
+    "url": "/static/css/main.128fadd7.chunk.css"
+  },
+  {
+    "revision": "ee544989136267f8986d",
+    "url": "/static/js/2.f867684e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f867684e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98453eddcfa6f15aeffa",
+    "url": "/static/js/main.8f095cc2.chunk.js"
+  },
+  {
+    "revision": "71257305934b9c18514a",
+    "url": "/static/js/runtime-main.0e10a3df.js"
+  },
+  {
+    "revision": "b69ded744bbb6411df0c0eea7853333a",
+    "url": "/static/media/background.b69ded74.png"
+  },
+  {
+    "revision": "d65ef50276d99f5428f00208edc0b0ee",
+    "url": "/static/media/classic.d65ef502.png"
+  },
+  {
+    "revision": "aff9083841f568b30ec9ab6662d92436",
+    "url": "/static/media/cleric.aff90838.png"
+  },
+  {
+    "revision": "33c51ce29b81e2a15c06ef9094eaea5a",
+    "url": "/static/media/empty.33c51ce2.png"
+  },
+  {
+    "revision": "86667da23b3db424e66d032953ba942e",
+    "url": "/static/media/fire.86667da2.png"
+  },
+  {
+    "revision": "2827cf70ee4359df5eaa9529c50407c4",
+    "url": "/static/media/gameFieldBackground.2827cf70.png"
+  },
+  {
+    "revision": "f99c53f73ac7b48c6318d891e1e47b2c",
+    "url": "/static/media/ragnaros.f99c53f7.png"
+  },
+  {
+    "revision": "d7fb95cbf72bcb0fdaed7742f53a42df",
+    "url": "/static/media/rainbow.d7fb95cb.png"
+  }
+]);
